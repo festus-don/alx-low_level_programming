ad83d19b75f81a3d60e13981578d31ea5bc4670c
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-08 11:51:14 -0700

    coding task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc puts"Programming is like building a multilingual puzzle\n"
+#include <stdio.h>
+/*this programs prints the text used by put function*/
+int main {
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+
