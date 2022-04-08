f6ef36e542fdc97f1a40ed1eb9d882da2f134ac4
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-08 12:10:40 -0700

    fess prof

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- * main-entry point
- * return (0): always success
+ *main- entry point
+ *
+ * return -always success 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
 
