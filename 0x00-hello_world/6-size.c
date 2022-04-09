61e0d12df0364056d61f7b4a29999bb805fa7d73
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-09 03:18:57 -0700

    fess

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0 (success)
 */
-
 int main(void)
 {
 	int a;
@@ -14,10 +13,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(d));
-	printf("size  of a int: %lu byte(s)\n", (unsigned long) sizeof(a));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+	printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(d));
+	printf("size  of a int: %1u byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %1u byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
