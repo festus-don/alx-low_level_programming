93ba283ddc27dfe366102533f003f8efe5e1b302
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-09 03:09:39 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point 
  *
  * Return: Always 0 (success)
 */
+
 int main(void)
 {
 	int a;
@@ -11,6 +13,7 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
+
 	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(d));
 	printf("size  of a int: %lu byte(s)\n", (unsigned long) sizeof(a));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
