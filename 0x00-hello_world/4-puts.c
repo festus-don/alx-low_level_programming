5f4d6b2fda5b4478dafae848af59445aa09a1adf
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-09 01:05:14 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * int main - it is the entry point
+ * main - it is the entry point
  *
  * return: always success 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
