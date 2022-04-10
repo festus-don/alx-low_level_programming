78f38252cd98579b539bcb0f06dd7adc2d547861
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-10 08:43:32 -0700

    last task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
