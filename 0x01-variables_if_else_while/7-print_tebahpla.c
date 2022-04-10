fa8b21c75a4577063789cfcd9af905681fe45b95
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-10 07:04:11 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	char h;
+
+	for (h = 'z'; h <= 'a'; ++h)
+	{
+
+		putchar(h);
+	}
+
+	putchar('\n);
+
+	return (0);
+}
