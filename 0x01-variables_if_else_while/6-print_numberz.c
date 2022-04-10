b05a9a1b4330634a2592e5626033053a39fa7198
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-10 06:39:46 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
