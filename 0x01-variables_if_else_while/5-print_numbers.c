507eaea8c80ff067067139eb3699f23de13cbcf5
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-10 05:40:56 -0700

    task 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 successs
+ */
+int main(void)
+{
+	int digit;
+
+	for(digit = 0; digit < 10; ++digit)
+	{
+		printf("%digit", digit);
+	}
+
+	printf("\n");
+
+	return (0);
+}
