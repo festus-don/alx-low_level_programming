ceefb257e0bd98cee8fc8c413dedb88d4e021b54
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-10 02:33:27 -0700

    second task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,19 +14,18 @@ int main(void)
 	Srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n == 0)
+	if (n % 10 > 5)
 	{
-		printf("%i is zero\n", n);
+		printf("Last digit of %d is %d and is grater than 5\n", n, n % 10);
 	}
-
-	else if (n < 0)
+	else if (n % 10 == 0)
 	{
-		printf("%i is negative\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 
 	else
 	{
-		printf("%i is zero\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 
 	return (0);
