caa8765e43849d7485548eafef41435b1c2bc976
Festus Kipkorir <festuskipkorir01@gmail.com>
2022-04-13 13:39:49 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
  * Return: Always 0 success
  */
 void print_alphabet_x10(void)
